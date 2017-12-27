7970bfc7f4cea1aa4826e8d5575ed82fa5bb1162
jackchow <jkzzit@163.com>
2017-12-28 01:19:50 +0800

    springboot

--- chapter4-thymeleaf/src/main/java/com/juzhen/Chapter4ThymeleafApplication.java
+++ chapter4-thymeleaf/src/main/java/com/juzhen/Chapter4ThymeleafApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter4ThymeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Chapter4ThymeleafApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter4ThymeleafApplication.class, args);
+    }
 }
